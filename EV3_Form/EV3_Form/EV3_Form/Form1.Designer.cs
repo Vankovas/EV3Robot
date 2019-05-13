a9772cd40eeeeef191ad73039887c7911f60fde0
Ivan Vasilev <vannkovas@gmail.com>
2019-05-13 14:16:10 +0200

    Migrated from GitLab

---
+++ EV3_Form/EV3_Form/EV3_Form/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace EV3_Form {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.portListBox = new System.Windows.Forms.ListBox();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbStatus = new System.Windows.Forms.TextBox();
+            this.btnClaw = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEmergency = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnBackward = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label2.Location = new System.Drawing.Point(109, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Connection";
+            // 
+            // portListBox
+            // 
+            this.portListBox.BackColor = System.Drawing.Color.LightBlue;
+            this.portListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.portListBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.portListBox.ForeColor = System.Drawing.Color.SteelBlue;
+            this.portListBox.FormattingEnabled = true;
+            this.portListBox.ItemHeight = 15;
+            this.portListBox.Location = new System.Drawing.Point(14, 27);
+            this.portListBox.Name = "portListBox";
+            this.portListBox.Size = new System.Drawing.Size(260, 60);
+            this.portListBox.TabIndex = 6;
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.BackColor = System.Drawing.Color.LightBlue;
+            this.disconnectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.disconnectButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.disconnectButton.ForeColor = System.Drawing.Color.SteelBlue;
+            this.disconnectButton.Location = new System.Drawing.Point(104, 102);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(84, 32);
+            this.disconnectButton.TabIndex = 9;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = false;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.BackColor = System.Drawing.Color.LightBlue;
+            this.connectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectButton.ForeColor = System.Drawing.Color.SteelBlue;
+            this.connectButton.Location = new System.Drawing.Point(194, 102);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(93, 32);
+            this.connectButton.TabIndex = 11;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = false;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.BackColor = System.Drawing.Color.LightBlue;
+            this.refreshButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.refreshButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refreshButton.ForeColor = System.Drawing.Color.SteelBlue;
+            this.refreshButton.Location = new System.Drawing.Point(5, 102);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(93, 32);
+            this.refreshButton.TabIndex = 10;
+            this.refreshButton.Text = "Referesh ports";
+            this.refreshButton.UseVisualStyleBackColor = false;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(108, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Robot Status";
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.BackColor = System.Drawing.Color.LightBlue;
+            this.tbStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.tbStatus.Location = new System.Drawing.Point(5, 155);
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.ReadOnly = true;
+            this.tbStatus.Size = new System.Drawing.Size(282, 23);
+            this.tbStatus.TabIndex = 13;
+            // 
+            // btnClaw
+            // 
+            this.btnClaw.BackColor = System.Drawing.Color.LightBlue;
+            this.btnClaw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClaw.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClaw.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnClaw.Location = new System.Drawing.Point(67, 222);
+            this.btnClaw.Name = "btnClaw";
+            this.btnClaw.Size = new System.Drawing.Size(77, 32);
+            this.btnClaw.TabIndex = 14;
+            this.btnClaw.Text = "Claw Up";
+            this.btnClaw.UseVisualStyleBackColor = false;
+            this.btnClaw.Click += new System.EventHandler(this.btnClaw_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LightBlue;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnStart.Location = new System.Drawing.Point(95, 184);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(93, 32);
+            this.btnStart.TabIndex = 15;
+            this.btnStart.Text = "Start Robot";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnEmergency
+            // 
+            this.btnEmergency.BackColor = System.Drawing.Color.LightBlue;
+            this.btnEmergency.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmergency.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmergency.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnEmergency.Location = new System.Drawing.Point(82, 298);
+            this.btnEmergency.Name = "btnEmergency";
+            this.btnEmergency.Size = new System.Drawing.Size(116, 32);
+            this.btnEmergency.TabIndex = 16;
+            this.btnEmergency.Text = "Emergency Brake";
+            this.btnEmergency.UseVisualStyleBackColor = false;
+            this.btnEmergency.Click += new System.EventHandler(this.btnEmergency_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.LightBlue;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnExit.Location = new System.Drawing.Point(94, 260);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(93, 32);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "Stop Program";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnForward
+            // 
+            this.btnForward.BackColor = System.Drawing.Color.LightBlue;
+            this.btnForward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForward.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForward.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnForward.Location = new System.Drawing.Point(244, 253);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(30, 32);
+            this.btnForward.TabIndex = 18;
+            this.btnForward.Text = "↑";
+            this.btnForward.UseVisualStyleBackColor = false;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnBackward
+            // 
+            this.btnBackward.BackColor = System.Drawing.Color.LightBlue;
+            this.btnBackward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBackward.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackward.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnBackward.Location = new System.Drawing.Point(244, 291);
+            this.btnBackward.Name = "btnBackward";
+            this.btnBackward.Size = new System.Drawing.Size(30, 32);
+            this.btnBackward.TabIndex = 19;
+            this.btnBackward.Text = "↓";
+            this.btnBackward.UseVisualStyleBackColor = false;
+            this.btnBackward.Click += new System.EventHandler(this.btnBackward_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.BackColor = System.Drawing.Color.LightBlue;
+            this.btnLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLeft.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeft.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnLeft.Location = new System.Drawing.Point(24, 253);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(30, 32);
+            this.btnLeft.TabIndex = 20;
+            this.btnLeft.Text = "←";
+            this.btnLeft.UseVisualStyleBackColor = false;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.BackColor = System.Drawing.Color.LightBlue;
+            this.btnRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRight.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRight.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnRight.Location = new System.Drawing.Point(24, 291);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(30, 32);
+            this.btnRight.TabIndex = 21;
+            this.btnRight.Text = "→";
+            this.btnRight.UseVisualStyleBackColor = false;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button1.Location = new System.Drawing.Point(150, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 32);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Claw Down";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(294, 335);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnBackward);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEmergency);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnClaw);
+            this.Controls.Add(this.tbStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.portListBox);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(310, 374);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(310, 374);
+            this.Name = "Form1";
+            this.Text = "VANBOT Control Box";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox portListBox;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbStatus;
+        private System.Windows.Forms.Button btnClaw;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEmergency;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnBackward;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EV3_Form/EV3_Form/EV3_Form/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EV3MessengerLib;
+using System.IO.Ports;
+
+namespace EV3_Form {
+    public partial class Form1 : Form {
+        string temp = "";
+        int output = -1;
+
+        private EV3Messenger messenger;
+        public Form1() {
+            InitializeComponent();
+            messenger = new EV3Messenger();
+            fillSerialPortSelectionBoxWithAvailablePorts();
+            updateFormGUI();
+            timer1.Enabled = true;
+            timer1.Interval = 50;
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e) {
+            fillSerialPortSelectionBoxWithAvailablePorts();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e) {
+            if (portListBox.SelectedIndex > -1) {
+                string port = portListBox.SelectedItem.ToString().ToUpper();
+                if (messenger.Connect(port)) {
+                    updateFormGUI();
+                    MessageBox.Show("Connected!");
+                }
+                else {
+                    MessageBox.Show("Failed to connect to serial port " + port);
+                }
+            }
+            else {
+                MessageBox.Show("Please select a port for the bluetooth connection");
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e) {
+            messenger.Disconnect();
+            updateFormGUI();
+        }
+
+        private void fillSerialPortSelectionBoxWithAvailablePorts() {
+            String[] ports = SerialPort.GetPortNames();
+            Array.Sort(ports);
+
+            portListBox.Items.Clear();
+            foreach (String port in ports) {
+                portListBox.Items.Add(port);
+            }
+        }
+
+        private void updateFormGUI() {
+            if (messenger.IsConnected) {
+                refreshButton.Enabled = false;
+                connectButton.Enabled = false;
+                disconnectButton.Enabled = true;
+            }
+            else {
+                refreshButton.Enabled = true;
+                connectButton.Enabled = true;
+                disconnectButton.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            EV3Message msg = messenger.ReadMessage();
+            if (msg != null) temp = msg.ValueAsText;
+            if (!string.IsNullOrEmpty(temp)) output = int.Parse(temp);
+            if (output == 1) tbStatus.Text = "Running Follow Path Algorithm";
+            else if (output == 2) tbStatus.Text = "Running Object Pickup Algorithm";
+            else if (output == 3) tbStatus.Text = "Running DropOff Algorithm";
+            else if (output == 4) tbStatus.Text = "Running Return To Start Algorithm";
+            else if (output == 5) tbStatus.Text = "Finished";
+        }
+
+        private void btnEmergency_Click(object sender, EventArgs e) {
+            messenger.SendMessage("abc", "Stop");
+        }
+
+        private void btnClaw_Click(object sender, EventArgs e) {
+            messenger.SendMessage("abc", "Claw");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e) {
+            messenger.SendMessage("abc", "Start");
+            btnLeft.Visible = false;
+            btnRight.Visible = false;
+            btnForward.Visible = false;
+            btnBackward.Visible = false;
+        }
+
+        private void btnForward_Click(object sender, EventArgs e) {
+            messenger.SendMessage("abc", "Forward");
+        }
+
+        private void btnBackward_Click(object sender, EventArgs e) {
+            messenger.SendMessage("abc", "Backward");
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e) {
+            messenger.SendMessage("abc", "Right");
+        }
+
+        private void btnRight_Click(object sender, EventArgs e) {
+            messenger.SendMessage("abc", "Left");
+        }
+    }
+}
